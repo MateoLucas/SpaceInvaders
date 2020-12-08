7e7269fae2f551f2883a0f3295b85c30d4971f70
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-12-08 17:00:44 -0300

    reestructure el programa en 3 partes, backend, allegro y raspi

---
+++ SpaceInvaders/backend.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   backend.h
+ * Author: jandro1111
+ *
+ * Created on 8 de diciembre de 2020, 16:01
+ */
+
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BACKEND_H */
+
