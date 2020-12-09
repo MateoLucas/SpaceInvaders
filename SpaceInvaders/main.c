e07759aaee2508018775670b8b96aac1a7a456bf
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-12-08 21:07:05 -0300

    inicializa matriz

--- SpaceInvaders/main.c
+++ SpaceInvaders/main.c
@@ -2,7 +2,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor.\sleep(1);
  */
 
 /* 
@@ -18,18 +18,25 @@
 #include"backend.h"
 #include"hardware.h"
 #include"allegro.h"
-
+#include<unistd.h>
+#include<time.h>// uso srand(time(NULL)) para incializar
+//rand() % 10 + 1;10 rango 1 valor inicial
 int main()
 {
     int exit=0;
-    initallegro();//inicializa allegro
+    //initallegro();//inicializa allegro
     //meterle un define o algo
-    initraspi();//inicializa librerias raspi
+   //initraspi();//inicializa librerias raspi
     //meterle define
-   
-    do{
+    int nivel=15;
+    int a=0;
+    int space [LARGO][ANCHO];
+    inimat(space,a);//inicializo la matriz
+    ininav(space,nivel);
+    printmat(space);
+   /* do{
         
     }while((exit = exit_cond())!=0);
-
+*/
 return 0;
 }
