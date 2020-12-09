9a2f937c3e35800027c646fa9f207959e87c2d39
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-12-09 01:39:52 -0300

    ya mueve las naves enemigas

--- SpaceInvaders/backend.h
+++ SpaceInvaders/backend.h
@@ -19,14 +19,17 @@ extern "C" {
 #endif
 #define LARGO 16 //el largo de la matriz
 #define ANCHO 16
-#define ENEMY 1
+#define ENEMY 1//despues agrego mas tipos de naves
+#define ENEMYSHOT 2//tipos de naves entre 1 a 7
 #define PLAYER 9
-#define MURO 8   
+#define MURO 8
+#define DER 1
+#define IZQ 2
 int exit_cond(void);
 void inimat (int [][ANCHO],int );
 void printmat (int [][ANCHO]);
 void ininav(int[][ANCHO],int);
-
+int ciclo (int [][ANCHO],int);
 #ifdef __cplusplus
 }
 #endif
