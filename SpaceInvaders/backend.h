0d9211c447d72a334efb948b5a4dfe8e66ecdf4e
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-12-09 23:47:19 -0300

    backend casi terminado falta timer

--- SpaceInvaders/backend.h
+++ SpaceInvaders/backend.h
@@ -20,16 +20,29 @@ extern "C" {
 #define LARGO 16 //el largo de la matriz
 #define ANCHO 16
 #define ENEMY 1//despues agrego mas tipos de naves
-#define ENEMYSHOT 2//tipos de naves entre 1 a 7
+#define ENEMYSHOT 2//tipos de naves entre 1 a 5 el 2 es el q dispara
+#define NAVNOD 5//siempre se mueve de izq a der, ocupa 2 bloques
+#define PSHOT 7   
+#define ESHOT 6
 #define PLAYER 9
 #define MURO 8
 #define DER 1
 #define IZQ 2
-int exit_cond(void);
-void inimat (int [][ANCHO],int );
+#include<stdio.h>
+#include<time.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include<unistd.h>
+int exit_cond(int *);
+void inimat (int [][ANCHO] );
 void printmat (int [][ANCHO]);
-void ininav(int[][ANCHO],int);
-int ciclo (int [][ANCHO],int);
+void ininiv(int[][ANCHO],int);
+int ciclonaves (int [][ANCHO],int);
+void ciclodisp(int [][ANCHO],int*,int*);
+int verparams(int [][ANCHO],int);
+void navdisp(int [][ANCHO],int *,int *,int *);
+void pmov(int [][ANCHO],int *); 
+void matniv(int [][ANCHO]);
 #ifdef __cplusplus
 }
 #endif
